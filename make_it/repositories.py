eb4fe3c4c5d233cb1433b0c874cfc17360fa7d25
Ghost6818 <96416735+Ghost6818@users.noreply.github.com>
2023-03-19 19:24:35 +0100

    repository homework

--- make_it/repositories.py
+++ make_it/repositories.py
@@ -1,3 +1,12 @@
 class UserRepository:
-    def add(self) -> None:
-        raise NotImplementedError
+    def add(self, user):
+        raise NotImplementedError()
+
+    def get(self, user_id):
+        raise NotImplementedError()
+
+    def update(self, user_id, update_data):
+        raise NotImplementedError()
+
+    def delete(self, user_id):
+        raise NotImplementedError()
--- tests/unit/test_repositories.py
+++ tests/unit/test_repositories.py
@@ -5,13 +5,27 @@ import pytest
 @pytest.fixture
 def repository() -> UserRepository:
     return UserRepository()
+class TestUserRepository:
+    def test_can_instantiate_user_repository(self):
+        repository = UserRepository()
+        assert isinstance(repository, UserRepository)
 
+    def test_user_repository_has_add_method(self):
+        repository = UserRepository()
+        with pytest.raises(NotImplementedError):
+            repository.add({})
 
-def test_can_instantiate_user_repository(
-    repository: UserRepository, ) -> None:
-    UserRepository()
+    def test_user_repository_has_get_method(self):
+        repository = UserRepository()
+        with pytest.raises(NotImplementedError):
+            repository.get(1)
 
+    def test_user_repository_has_update_method(self):
+        repository = UserRepository()
+        with pytest.raises(NotImplementedError):
+            repository.update(1, {})
 
-def test_user_repository_has_add_method(repository: UserRepository):
-    with pytest.raises(NotImplementedError):
-        repository.add()
+    def test_user_repository_has_delete_method(self):
+        repository = UserRepository()
+        with pytest.raises(NotImplementedError):
+            repository.delete(1)
