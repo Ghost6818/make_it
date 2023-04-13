ab80fe952848d53d7b89da05d18b51cb740cbc6d
Ghost6818 <96416735+Ghost6818@users.noreply.github.com>
2023-04-13 13:22:55 +0200

    more on lesson

--- make_it/views.py
+++ make_it/views.py
@@ -7,7 +7,10 @@ from make_it.controllers import GetUserController
 class UserView(MethodView):
     def __init__(self, controller: GetUserController) -> None:
         self._get_user_controller = controller
-    def get(self, id: str) -> Response:
-        return Response(status=501)
-    #wywołać controller w get
 
+    def get(self, id: str) -> Response:
+        try:
+            self._get_user_controller.get(int(id))
+        except NotImplementedError:
+            pass
+        return Response(status=501)
